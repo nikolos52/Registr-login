9787b0686e83ec4c54dafd71e865fcff3ac474a3
Nazar Tertyshnyi <nazar.tertyshny@gmail.com>
2020-02-24 19:27:35 +0200

    Добавлена папка компонентов

---
+++ components/signup.php
@@ -0,0 +1,15 @@
+<form action="check.php" method="POST">
+	<!-- Общий совет - не забывай про атрибут required -->
+	<input type="email" name="email" placeholder="Введите EMail" required>
+
+	<input type="password" name="password" placeholder="Введите пароль"><br>
+
+	<input type="text" name="last_name" placeholder="Введите фамилию"><br>
+
+	<input type="text" name="first_name" placeholder="Введите имя"><br>
+
+	<input type="text" name="middle_name" placeholder="Введите отчество"><br>
+
+	<!-- Не забывай указывать тип кнопки -->
+	<button type="submit">Дальше</button>
+</form>
